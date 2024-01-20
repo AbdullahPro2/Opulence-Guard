1739804d70404ae9b0520ad810ba7f79afa316c8
AbdullahPro2 <zeb16208@gmail.com>
2024-01-20 05:11:01 +0100

    deploying site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Opulence-Guard/',
   plugins: [react(), eslint()],
 });
